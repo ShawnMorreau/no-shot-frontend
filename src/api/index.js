e01de7f1b3e618b5cf7be7cf7e62e8ec6dcee6fa
ShawnMorreau <Shawn.morreau@hotmail.com>
2021-08-01 03:58:47 -0400

    switch server

--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,26 @@
-var socket = new WebSocket("ws://localhost:5000/ws")
-// var socket = new WebSocket("wss://murmuring-temple-10804.herokuapp.com/ws") 
-let connect = cb => {
-    socket.onopen = () => {
-        console.log("Connected to server")
-    }
+// var socket = new WebSocket("ws://localhost:5000/ws")
+// var socket = new WebSocket("wss://murmuring-temple-10804.herokuapp.com/ws")
+var socket = new WebSocket("wss://morning-refuge-68385.herokuapp.com/ws");
+let connect = (cb) => {
+  socket.onopen = () => {
+    console.log("Connected to server");
+  };
 
-    socket.onmessage = msg => {
-        cb(msg.data);
-    }
+  socket.onmessage = (msg) => {
+    cb(msg.data);
+  };
 
-    socket.onclose = event => {
-        console.log("Socket Connection Closed", event);
-    }
+  socket.onclose = (event) => {
+    console.log("Socket Connection Closed", event);
+  };
 
-    socket.onerror = error => {
-        console.log("Socket Error: ", error)
-    }
+  socket.onerror = (error) => {
+    console.log("Socket Error: ", error);
+  };
 };
 
-let sendMsg = msg => {
-    socket.send(msg)
-}
+let sendMsg = (msg) => {
+  socket.send(msg);
+};
 
-export {connect, sendMsg}
+export { connect, sendMsg };
